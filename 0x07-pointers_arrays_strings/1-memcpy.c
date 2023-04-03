feaaa4f4795db39268c08f4132a9df73568f1458
Haffie023 <joyce.muchoki@gmail.com>
2023-04-03 13:54:00 -0700

    1-memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,16 +3,14 @@
 /**
  *_memcpy - function that copies memory area
  * @dest: the address of memory to print
- * @src: second value
- * @n: third value
+ * @src: source of memory
+ * @n: number of bytes
  *Return: char with result of memset
  */
 char *_memcpy(char *dest, char *src, unsigned int n);
 {
 	unsigned int i;
-	
 	for(;i < n; i = 0; i++);
-
 	{
 		dest[i] = src[i];
 	}
