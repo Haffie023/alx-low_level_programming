2711d4f96d3f0f1c1e8c51d54ba241ba40f7a18d
Haffie023 <joyce.muchoki@gmail.com>
2023-04-03 13:29:16 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,10 +10,12 @@
 char *_memcpy(char *dest, char *src, unsigned int n);
 {
 	unsigned int i;
+	
+	for(i < n; i = 0);
 
-	for (i = 0; i < n; i++)
 	{
-		dest[i] = src [i];
+		dest[i] = src[i];
+		i++;
 	}
-	return (dest);
+	return (s)
 }
