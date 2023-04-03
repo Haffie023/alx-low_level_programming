8de39d46f63214fecce357dea034864427c0ab6e
Haffie023 <joyce.muchoki@gmail.com>
2023-04-03 12:29:16 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,3 @@
-#ifndef MAIN_H
 #define MAIN_H
 
 char *_memset(char *s, char b, unsigned int n);
@@ -10,5 +9,3 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
-#endif 
