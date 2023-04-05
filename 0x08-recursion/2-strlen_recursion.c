ccc515809d9e5ccbb36db2ebacf074d0ef2caedb
Haffie023 <joyce.muchoki@gmail.com>
2023-04-05 11:37:18 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,14 +2,11 @@
 /**
  * _strlen_recursion - prints the length of a string
  * @s:the string
- * Returns:l length of string as an int
+ * Return: int
  */
 int _strlen_recursion(char *s)
 {
-	if (*s)
-	{
-		if (*s == '\0')
+	if (*s == '\0')
 		return (0);
-	}
 	return (1 + _strlen_recursion(s + 1));
 }
