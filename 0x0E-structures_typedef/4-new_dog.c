162964d4f12ce13583eb35895036ec4458c4e8a5
Haffie023 <joyce.muchoki@gmail.com>
2023-04-17 07:24:21 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,11 +6,12 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
+ *
  * Return: pointer to new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	unsigned int n1, n2, i;
+	unsigned int nl, ol, i;
 	dog_t *dog;
 
 	if (name == NULL || owner == NULL)
@@ -20,7 +21,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	for (nl = 0; name[nl]; nl++)
 		;
-	n1++;
+	nl++;
 	dog->name = malloc(nl * sizeof(char));
 	if (dog->name == NULL)
 	{
@@ -30,17 +31,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0; i < nl; i++)
 		dog->name[i] = name[i];
 	dog->age = age;
-	for (n2 = 0; owner[n2]; n2++)
+	for (ol = 0; owner[ol]; ol++)
 		;
-	n2++;
-	dog->owner = malloc(n2 * sizeof(char));
+	ol++;
+	dog->owner = malloc(ol * sizeof(char));
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-	for (i = 0; i < n2; i++)
+	for (i = 0; i < ol; i++)
 		dog->owner[i] = owner[i];
 	return (dog);
 }
