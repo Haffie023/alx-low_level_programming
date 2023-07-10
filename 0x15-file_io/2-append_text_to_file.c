5f706676a40c0a83fa0b736a889fa19a9f798824
Haffie023 <joyce.muchoki@gmail.com>
2023-07-10 11:17:08 -0700

    file-io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,41 +1,42 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * append_text_to_file - Append text at the end of a file
- * @filename: name of the file to create.
- * @text_content: text to write inside the file.
- *
- * Return: 1 if text_content was appened, -1 otherwise.
+ *append_text_to_file - text append
+ *@filename: file name
+ *@text_content: write file
+ *Return: 1 on success and -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd_open, fd_write;
+	int a, b, c;
+	char *buf;
 
 	if (filename == NULL)
 		return (-1);
-	fd_open = open(filename, O_RDWR | O_APPEND);
-	if (fd_open == -1)
-		return (-1);
-	if (text_content != NULL)
+
+	if (text_content == NULL)
 	{
-		fd_write = write(fd_open, text_content, _strlen(text_content));
-		if (fd_write == -1)
+		a = open(filename, O_CREAT, 0600);
+		if (a == -1)
 			return (-1);
+		return (1);
 	}
-	close(fd_open);
+
+	for (c = 0; text_content[c] != '\0'; c++)
+		;
+	buf = malloc(c * sizeof(char));
+	if (buf == NULL)
+		return (-1);
+
+	a = open(filename, O_APPEND | O_WRONLY);
+	if (a == -1)
+		return (-1);
+
+	b = write(a, text_content, c);
+	if (b == -1)
+		return (-1);
+
+	close(a);
+	free(buf);
 	return (1);
-}
-
-/**
- * _strlen - calculate the lenght of a string.
- * @str: array of characters.
- *
- * Return: lenght of the string.
- */
-int _strlen(char *str)
-{
-	int count = 0;
-
-	while (*str++)
-		count++;
-	return (count);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,5 +15,15 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _strlen(char *str);
 void close_file(int FD);
-
+void handle_exit(char *message);
+void is_elf_file(Elf32_Ehdr *h);
+void display_elf_magic(Elf32_Ehdr *h);
+void display_elf_class(unsigned char class);
+void display_elf_data(unsigned char data);
+void display_elf_version(unsigned char version);
+void display_elf_osabi(unsigned char osabi);
+void display_elf_type(uint16_t type, int be);
+void display_elf_entry(Elf64_Addr addr, int be);
+void display_elf(Elf32_Ehdr *h);
+int main(int ac, char **av);
 #endif
