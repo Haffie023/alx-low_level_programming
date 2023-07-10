bf8f1151193cb7680632b1c77f3cb6c8f2dd8dd6
Haffie023 <joyce.muchoki@gmail.com>
2023-07-10 11:31:05 -0700

    appended

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,28 +1,34 @@
 #include "main.h"
 
 /**
- * append_text_to_file - appends text at the end of a file
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: A pointer to the name of the file.
+ * @text_content: The string to add to the end of the file.
  *
- * @filename: name of the file
- * @text_content: text to add to the end of the file
- *
- * Return: 1 on success, -1 on failure (NULL filename, bad permissions)
+ * Return: If the function fails or filename is NULL - -1.
+ *         If the file does not exist the user lacks write permissions - -1.
+ *         Otherwise - 1.
  */
-
-int	append_text_to_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd = open(filename, O_WRONLY | O_APPEND), len = 0;
-	ssize_t write_o = 0;
+	int o, w, len = 0;
 
-	if (fd > -1 && filename)
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
 	{
-		while (text_content && text_content[len])
+		for (len = 0; text_content[len];)
 			len++;
-		if (len > 0)
-			write_o = write(fd, text_content, len);
-		close(fd);
-		if (write_o > -1)
-			return (1);
 	}
-	return (-1);
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <unistd.h>
 #include <stdio.h>
@@ -7,23 +7,10 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#define BUF_SIZE 1024
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int _strlen(char *str);
-void close_file(int FD);
-void handle_exit(char *message);
-void is_elf_file(Elf32_Ehdr *h);
-void display_elf_magic(Elf32_Ehdr *h);
-void display_elf_class(unsigned char class);
-void display_elf_data(unsigned char data);
-void display_elf_version(unsigned char version);
-void display_elf_osabi(unsigned char osabi);
-void display_elf_type(uint16_t type, int be);
-void display_elf_entry(Elf64_Addr addr, int be);
-void display_elf(Elf32_Ehdr *h);
-int main(int ac, char **av);
+
 #endif
