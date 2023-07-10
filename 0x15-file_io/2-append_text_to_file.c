8be2bf56771f4e73b08e9ac317219f3142eab4e5
Haffie023 <joyce.muchoki@gmail.com>
2023-07-10 11:21:16 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,42 +1,28 @@
 #include "main.h"
 
 /**
- *append_text_to_file - text append
- *@filename: file name
- *@text_content: write file
- *Return: 1 on success and -1 on failure
+ * append_text_to_file - appends text at the end of a file
+ *
+ * @filename: name of the file
+ * @text_content: text to add to the end of the file
+ *
+ * Return: 1 on success, -1 on failure (NULL filename, bad permissions)
  */
-int append_text_to_file(const char *filename, char *text_content)
+
+int	append_text_to_file(const char *filename, char *text_content)
 {
-	int a, b, c;
-	char *buf;
+	int fd = open(filename, O_WRONLY | O_APPEND), len = 0;
+	ssize_t write_o = 0;
 
-	if (filename == NULL)
-		return (-1);
-
-	if (text_content == NULL)
+	if (fd > -1 && filename)
 	{
-		a = open(filename, O_CREAT, 0600);
-		if (a == -1)
-			return (-1);
-		return (1);
+		while (text_content && text_content[len])
+			len++;
+		if (len > 0)
+			write_o = write(fd, text_content, len);
+		close(fd);
+		if (write_o > -1)
+			return (1);
 	}
-
-	for (c = 0; text_content[c] != '\0'; c++)
-		;
-	buf = malloc(c * sizeof(char));
-	if (buf == NULL)
-		return (-1);
-
-	a = open(filename, O_APPEND | O_WRONLY);
-	if (a == -1)
-		return (-1);
-
-	b = write(a, text_content, c);
-	if (b == -1)
-		return (-1);
-
-	close(a);
-	free(buf);
-	return (1);
+	return (-1);
 }
